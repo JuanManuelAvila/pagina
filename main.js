fb59ed09dd077b2789d4cf494f202c76cf344af5
JuanManuelAvila <halcon18d@gmail.com>
2021-04-29 13:34:20 -0500

    agregamos navbar y mas estilo a la pagina

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 let btnItems = document.querySelectorAll(".item .btn-item");
 for (let i = 0; i < btnItems.length; i++)
-{
+{    
     btnItems[i].addEventListener("click", function (e)
     {        
         let btn = e.target;
 
-        if (btn.className == "btn-item active") {
+        if (btn.className == "btn-item active")
+        {
             btn.classList.remove("active")
         }
-        else {
+        else
+        {
             btn.classList.add("active")            
         }
     })
