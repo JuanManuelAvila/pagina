bd634804ee8d4892f40dd62f21eee4bf59574647
Juan Avila <halcon18d@gmail.com>
2021-05-05 08:49:01 -0500

    creacion de targeta de reproduccion de audio

---
+++ audio.js
@@ -0,0 +1,46 @@
+var timer;
+var percent = 0;
+var audio = document.getElementById("audio");
+audio.addEventListener("playing", function(_event) {
+  var duration = _event.target.duration;
+  advance(duration, audio);
+});
+audio.addEventListener("pause", function(_event) {
+  clearTimeout(timer);
+});
+var advance = function(duration, element) {
+  var progress = document.getElementById("progress");
+  increment = 10/duration
+  percent = Math.min(increment * element.currentTime * 10, 100);
+  progress.style.width = percent+'%'
+  startTimer(duration, element);
+}
+var startTimer = function(duration, element){ 
+  if(percent < 100) {
+    timer = setTimeout(function (){advance(duration, element)}, 100);
+    
+    
+  }
+}
+
+function togglePlay (e) {
+  e = e || window.event;
+  var btn = e.target;
+  if (!audio.paused) {
+    btn.classList.remove('active');
+    audio.pause();
+    isPlaying = false;
+  } else {
+    btn.classList.add('active');
+    audio.play();
+    isPlaying = true;
+  }
+}
+
+function secondsToHms(timestamp) {
+        var hours = Math.floor(timestamp / 60 / 60);
+        var minutes = Math.floor((timestamp / 60) - (hours * 60));
+        var seconds = Math.floor(timestamp % 60);
+        var formatted = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+        return formatted;
+}
