49e50eada3a78b8feb52988dea71e5c99327e1d5
Juan Avila <halcon18d@gmail.com>
2021-05-27 16:10:36 -0500

    agregado audios de primeros

--- Assets/StreamingAssets/xampp/htdocs/audio.js
+++ Assets/StreamingAssets/xampp/htdocs/audio.js
@@ -1,6 +1,7 @@
 var timer;
 var percent = 0;
-var audio = document.getElementById("audio");
+var audio = document.getElementById("audio_1");//audio
+
 audio.addEventListener("playing", function(_event) {
   var duration = _event.target.duration;
   advance(duration, audio);
@@ -9,23 +10,22 @@ audio.addEventListener("pause", function(_event) {
   clearTimeout(timer);
 });
 var advance = function(duration, element) {
-  var progress = document.getElementById("progress");
+  var progress = document.getElementById("progress_1");//barra
   increment = 10/duration
   percent = Math.min(increment * element.currentTime * 10, 100);
   progress.style.width = percent+'%'
   startTimer(duration, element);
 }
 var startTimer = function(duration, element){ 
-  if(percent < 100) {
-    timer = setTimeout(function (){advance(duration, element)}, 100);
-    
-    
+  if (percent < 100)
+  {
+    timer = setTimeout(function (){advance(duration, element)}, 100);   
   }
 }
 
 function togglePlay (e) {
   e = e || window.event;
-  var btn = e.target;
+  var btn = e.target;  
   if (!audio.paused) {
     btn.classList.remove('active');
     audio.pause();
