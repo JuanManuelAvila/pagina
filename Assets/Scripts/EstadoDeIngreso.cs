48c051c38efd946fd624bf6cf0f932d585707223
Juan Avila <halcon18d@gmail.com>
2021-06-03 07:59:11 -0500

    agregados de audios en ingles y verificador de licencia

--- Assets/Scripts/EstadoDeIngreso.cs
+++ Assets/Scripts/EstadoDeIngreso.cs
@@ -1,15 +1,171 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+using System.Threading.Tasks;
 
 public class EstadoDeIngreso : MonoBehaviour
-{    
+{
+
+    public List<string> lincencias, estados;
+    public List<int> rangosFecha;
+    public GameObject pantallaLicencia;
+    public Button botonActivar;
+    public InputField textoLicencia;
+    public Text mensajeLicencia,estadoLicencia;
+    
+
+    public int estado= 0;
     int primerEstado = 0;
-    bool startXampp = false;
-    // Start is called before the first frame update
+    bool startXampp = false;    
+    public int diaActual, mesActual, añoActual;
+    public int diaActivacion, mesActivacion, añoActivacion;
+    public int diaDesactivacion, mesDesactivacion, añoDesactivacion;
+    
+
+    private void Awake()
+    {
+        LoadFileInfo();
+        LoadFileLicense();
+        diaActual = DateTime.Now.Day;
+        mesActual = DateTime.Now.Month;
+        añoActual = DateTime.Now.Year;
+
+        estado = PlayerPrefs.GetInt("estado");
+
+        if (estado == 1)
+        {
+            pantallaLicencia.SetActive(false);
+        }
+        else {
+            pantallaLicencia.SetActive(true);
+        }
+
+        botonActivar.onClick.AddListener(ActivarLicencia);            
+    }
+
+
+    public void LoadFileLicense() {
+
+
+        string fileName = Application.dataPath + "/StreamingAssets/xampp/licenses/apache/license.txt";
+
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lincencias.Clear();
+                estados.Clear();
+                string[] lines = File.ReadAllLines(fileName);
+                for (int x = 0; x < lines.Length; x++)
+                {
+                    string[] subs = lines[x].Split(' ');
+                    lincencias.Add(subs[0]);
+                    estados.Add(subs[1]);
+                }
+            }
+        }
+    }
+
+    
+    public void LoadFileInfo()
+    {
+        string fileName = Application.dataPath + "/StreamingAssets/xampp/licenses/apache/info.txt";
+        string[] lines = File.ReadAllLines(fileName);
+
+        for (int line = 0; line < lines.Length; line++) {
+            string secondLine = lines[1];
+            string[] subs = secondLine.Split(':', '-');
+            for (int j = 0; j < subs.Length; j++)
+            {
+                diaDesactivacion = int.Parse(subs[1]);
+                mesDesactivacion = int.Parse(subs[2]);
+                añoDesactivacion = int.Parse(subs[3]);
+            }
+        }
+    }
+
+
+    public void WriteFileInfo()
+    {
+
+        string fileName = Application.dataPath + "/StreamingAssets/xampp/licenses/apache/info.txt";
+        System.IO.File.WriteAllText(fileName, string.Empty);
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            writer.WriteLine("Activacion:" + diaActivacion + "-" + mesActivacion + "-" + añoActivacion);
+            writer.WriteLine("Vencimiento:" + diaDesactivacion + "-" + mesDesactivacion + "-" + añoDesactivacion);
+        }
+        // Read a file  
+        string readText = File.ReadAllText(fileName);
+    }
+
+    public void WriteFileLicense() {
+
+        string fileName = Application.dataPath + "/StreamingAssets/xampp/licenses/apache/license.txt";
+        System.IO.File.WriteAllText(fileName, string.Empty);
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            for (int i = 0; i< lincencias.Count; i++) {
+                writer.WriteLine(lincencias[i]+ " "+estados[i]);
+            }
+        }
+        // Read a file  
+        string readText = File.ReadAllText(fileName);
+    }
+
+    
+
+    public void ActivarLicencia() {
+        if (estado == 0)
+        {
+            if (lincencias.Contains(textoLicencia.text))
+            {
+                int index = lincencias.IndexOf(textoLicencia.text);
+                lincencias.Remove(textoLicencia.text);
+                estados.RemoveAt(index);
+
+                WriteFileLicense();
+                estado = 1;
+                diaActivacion = DateTime.Now.Day;
+                mesActivacion = DateTime.Now.Month;
+                añoActivacion = DateTime.Now.Year;
+
+                diaDesactivacion = DateTime.Now.Day;
+                mesDesactivacion = DateTime.Now.Month;
+                añoDesactivacion = (DateTime.Now.Year + 1);
+
+                WriteFileInfo();
+                PlayerPrefs.SetInt("estado", 1);
+                StartCoroutine(delayPantalla());
+
+
+            }
+            else {
+                mensajeLicencia.text = "Licencia no existe";
+                textoLicencia.text = "";
+                StartCoroutine(delayMensaje());
+            }
+        }
+    }
+
+    IEnumerator delayPantalla()
+    {
+        yield return new WaitForSeconds(2f);
+        mensajeLicencia.text = "";
+        pantallaLicencia.SetActive(false);
+    }
+
+    IEnumerator delayMensaje() {
+        yield return new WaitForSeconds(2f);
+        mensajeLicencia.text = "";
+    }
+
     void Start()
     {
         startXampp = false;
@@ -26,9 +182,53 @@ public class EstadoDeIngreso : MonoBehaviour
     private void FixedUpdate()
     {
         primerEstado = PlayerPrefs.GetInt("primer ingreso");
-        transform.GetChild(0).GetComponent<Button>().interactable = primerEstado != 0;
-        transform.GetChild(1).GetComponent<Button>().interactable = primerEstado == 0;
-        transform.GetChild(2).GetComponent<Button>().interactable = primerEstado != 0;       
+        
+        if (estado == 0)
+        {
+            if (textoLicencia.text == "")
+            {
+                botonActivar.interactable = false;
+            }
+            else
+            {
+                botonActivar.interactable = true;
+            }
+            estadoLicencia.text = "Estado licencia: Inactiva";
+        }
+        else {
+            diaActual = DateTime.Now.Day;
+            mesActual = DateTime.Now.Month;
+            añoActual = DateTime.Now.Year;
+
+            
+
+            if (rangosFecha.Contains(añoActual))
+            {
+                if (diaActual >= diaDesactivacion &&
+                                mesActual >= mesDesactivacion &&
+                                añoActual >= añoDesactivacion)
+                {
+                    pantallaLicencia.SetActive(true);
+                    estado = 0;
+                    mensajeLicencia.text = "La licencia ha caducado";
+                }
+                estadoLicencia.text = "Estado licencia: Activa";
+                transform.GetChild(0).GetComponent<Button>().interactable = primerEstado != 0;
+                transform.GetChild(1).GetComponent<Button>().interactable = primerEstado == 0;
+                transform.GetChild(2).GetComponent<Button>().interactable = primerEstado != 0;
+            }
+            else {
+                estadoLicencia.text = "Tiempo de uso ha experidado";
+
+                transform.GetChild(0).GetComponent<Button>().interactable = false;
+                transform.GetChild(1).GetComponent<Button>().interactable = false;
+                transform.GetChild(2).GetComponent<Button>().interactable = false;
+
+            }
+
+            
+        }
+
     }
     void primerClick()
     {
--- Assets/StreamingAssets/xampp/htdocs/audio.js
+++ Assets/StreamingAssets/xampp/htdocs/audio.js
@@ -1,147 +1,155 @@
 var timer;
 var percent = 0;
-var audio_1 = document.getElementById("audio_1");//audio
-var audio_2 = document.getElementById("audio_2");//audio
-var audio_3 = document.getElementById("audio_3");//audio
-var audio_4 = document.getElementById("audio_4");//audio
-var audio_5 = document.getElementById("audio_5");//audio
-var audio_6 = document.getElementById("audio_6");//audio
-var audio_7 = document.getElementById("audio_7");//audio
-var audio_8 = document.getElementById("audio_8");//audio
-var audio_9 = document.getElementById("audio_9");//audio
-var audio_10 = document.getElementById("audio_10");//audio
-var audio_11 = document.getElementById("audio_11");//audio
-var audio_12 = document.getElementById("audio_12");//audio
-var audio_13 = document.getElementById("audio_13");//audio
-var audio_14 = document.getElementById("audio_14");//audio
-var audio_15 = document.getElementById("audio_15");//audio
-var audio_16 = document.getElementById("audio_16");//audio
-var audio_17 = document.getElementById("audio_17");//audio
+var audio_1 = document.getElementById("audio_1"); //audio
+var audio_2 = document.getElementById("audio_2"); //audio
+var audio_3 = document.getElementById("audio_3"); //audio
+var audio_4 = document.getElementById("audio_4"); //audio
+var audio_5 = document.getElementById("audio_5"); //audio
+var audio_6 = document.getElementById("audio_6"); //audio
+var audio_7 = document.getElementById("audio_7"); //audio
+var audio_8 = document.getElementById("audio_8"); //audio
+var audio_9 = document.getElementById("audio_9"); //audio
+var audio_10 = document.getElementById("audio_10"); //audio
+var audio_11 = document.getElementById("audio_11"); //audio
+var audio_12 = document.getElementById("audio_12"); //audio
+var audio_13 = document.getElementById("audio_13"); //audio
+var audio_14 = document.getElementById("audio_14"); //audio
+var audio_15 = document.getElementById("audio_15"); //audio
+var audio_16 = document.getElementById("audio_16"); //audio
+var audio_17 = document.getElementById("audio_17"); //audio
+var audio_18 = document.getElementById("audio_18"); //audio
 
 //var audios = ["audio_1", "audio_2", "audio_3"]
 
-audio_1.addEventListener("playing", function(_event) {
+audio_1.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_1);
 });
-audio_1.addEventListener("pause", function(_event) {
+audio_1.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
 
-audio_2.addEventListener("playing", function(_event) {
+audio_2.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_2);
 });
-audio_2.addEventListener("pause", function(_event) {
+audio_2.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
 
-audio_3.addEventListener("playing", function(_event) {
+audio_3.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_3);
 });
-audio_3.addEventListener("pause", function(_event) {
+audio_3.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_4.addEventListener("playing", function(_event) {
+audio_4.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_4);
 });
-audio_4.addEventListener("pause", function(_event) {
+audio_4.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_5.addEventListener("playing", function(_event) {
+audio_5.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_5);
 });
-audio_5.addEventListener("pause", function(_event) {
+audio_5.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_6.addEventListener("playing", function(_event) {
+audio_6.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_6);
 });
-audio_6.addEventListener("pause", function(_event) {
+audio_6.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_7.addEventListener("playing", function(_event) {
+audio_7.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_7);
 });
-audio_7.addEventListener("pause", function(_event) {
+audio_7.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_8.addEventListener("playing", function(_event) {
+audio_8.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_8);
 });
-audio_8.addEventListener("pause", function(_event) {
+audio_8.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_9.addEventListener("playing", function(_event) {
+audio_9.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_9);
 });
-audio_9.addEventListener("pause", function(_event) {
+audio_9.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_10.addEventListener("playing", function(_event) {
+audio_10.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_10);
 });
-audio_10.addEventListener("pause", function(_event) {
+audio_10.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_11.addEventListener("playing", function(_event) {
+audio_11.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_11);
 });
-audio_11.addEventListener("pause", function(_event) {
+audio_11.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_12.addEventListener("playing", function(_event) {
+audio_12.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_12);
 });
-audio_12.addEventListener("pause", function(_event) {
+audio_12.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_13.addEventListener("playing", function(_event) {
+audio_13.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_13);
 });
-audio_13.addEventListener("pause", function(_event) {
+audio_13.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_14.addEventListener("playing", function(_event) {
+audio_14.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_14);
 });
-audio_14.addEventListener("pause", function(_event) {
+audio_14.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_15.addEventListener("playing", function(_event) {
+audio_15.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_15);
 });
-audio_15.addEventListener("pause", function(_event) {
+audio_15.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
-audio_16.addEventListener("playing", function(_event) {
+audio_16.addEventListener("playing", function (_event) {
   var duration = _event.target.duration;
   advance(duration, audio_16);
 });
-audio_16.addEventListener("pause", function(_event) {
-  clearTimeout(timer);
-});
-audio_17.addEventListener("playing", function(_event) {
-  var duration = _event.target.duration;
-  advance(duration, audio_17);
-});
-audio_17.addEventListener("pause", function(_event) {
+audio_16.addEventListener("pause", function (_event) {
   clearTimeout(timer);
 });
 
+audio_17.addEventListener("playing", function (_event) {
+  var duration = _event.target.duration;
+  advance(duration, audio_17);
+});
+audio_17.addEventListener("pause", function (_event) {
+  clearTimeout(timer);
+});
+audio_18.addEventListener("playing", function (_event) {
+  var duration = _event.target.duration;
+  advance(duration, audio_18);
+});
+audio_18.addEventListener("pause", function (_event) {
+  clearTimeout(timer);
+});
 
 //Inicio();
 
@@ -160,68 +168,71 @@ audio_17.addEventListener("pause", function(_event) {
 //     });
 //   }
 // }
-var advance = function (duration, element) {    
+var advance = function (duration, element) {
   var padreAudio = element.closest("div > div");
   var padre = padreAudio.parentNode;
   var nodeList = padre.childNodes;
   var progress = nodeList[5];
-  
-  increment = 10/duration;
+
+  increment = 10 / duration;
   percent = Math.min(increment * element.currentTime * 10, 100);
-  progress.style.width = percent+'%'
+  progress.style.width = percent + "%";
   startTimer(duration, element);
-}
-var startTimer = function(duration, element){ 
-  if (percent < 100)
-  {
-    timer = setTimeout(function (){advance(duration, element)}, 100);   
+};
+var startTimer = function (duration, element) {
+  if (percent < 100) {
+    timer = setTimeout(function () {
+      advance(duration, element);
+    }, 100);
   }
-}
+};
 
 function togglePlay(e) {
-  
   e = e || window.event;
   var btn = e.target;
   allpause(btn);
   var audioId = btn.id;
   audio = document.getElementById(audioId);
   if (!audio.paused) {
-    btn.classList.remove('active');
+    btn.classList.remove("active");
     audio.pause();
     isPlaying = false;
   } else {
-    btn.classList.add('active');
+    btn.classList.add("active");
     audio.play();
     isPlaying = true;
   }
 }
 
-function allpause(e)
-{
-  for (var i = 1; i <= 17; i++) {
-    var audioAuxiliar = document.getElementById("audio_" + i);
-    var auxId = "audio_" + i;
-    if (e.id != auxId) {
-
-      var padreAudio = audioAuxiliar.closest("div > div");
-      var padre = padreAudio.parentNode;
-      var nodeList = padre.childNodes;
-      var progress = nodeList[7];
-      var controles = progress.childNodes;
-      var play = controles[3];
-      var playBtn = play.childNodes;
-      var imgPlay = playBtn[1];
-      //console.log(imgPlay);
-      imgPlay.classList.remove('active');
-      audioAuxiliar.pause();
+function allpause(e) {
+  for (var i = 1; i <= 18; i++) {
+    if (document.getElementById("audio_" + i) != null) {
+      var audioAuxiliar = document.getElementById("audio_" + i);
+      var auxId = "audio_" + i;
+      if (e.id != auxId) {
+        var padreAudio = audioAuxiliar.closest("div > div");
+        var padre = padreAudio.parentNode;
+        var nodeList = padre.childNodes;
+        var progress = nodeList[7];
+        var controles = progress.childNodes;
+        var play = controles[3];
+        var playBtn = play.childNodes;
+        var imgPlay = playBtn[1];
+        //console.log(imgPlay);
+        imgPlay.classList.remove("active");
+        audioAuxiliar.pause();
+      }
     }
   }
 }
 
 function secondsToHms(timestamp) {
-        var hours = Math.floor(timestamp / 60 / 60);
-        var minutes = Math.floor((timestamp / 60) - (hours * 60));
-        var seconds = Math.floor(timestamp % 60);
-        var formatted = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
-        return formatted;
+  var hours = Math.floor(timestamp / 60 / 60);
+  var minutes = Math.floor(timestamp / 60 - hours * 60);
+  var seconds = Math.floor(timestamp % 60);
+  var formatted =
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
+  return formatted;
 }
